a4e0e64f67ed6025b86dd95e931234174fb016b9
LorranFont <lorran0202fps@gmail.com>
2025-02-22 08:56:57 -0300

    Adicionando viewsets para produtos, vendas, estoques, usuários e ofertas

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Product, Sale, Stock, User, Offer
+from .serializers import ProductSerializer, SaleSerializer, StockSerializer, UserSerializer, OfferSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class OfferViewSet(viewsets.ModelViewSet):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
