37d8a456ab5b408fcd26716b836c7beb9069e77a
LorranFont <lorran0202fps@gmail.com>
2025-02-22 09:13:07 -0300

    Adicionando rotas para viewsets de produtos, vendas, estoques, usuários e ofertas

--- urls.py
+++ urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, SaleViewSet, StockViewSet, UserViewSet, OfferViewSet
 
+
+router = DefaultRouter()
+router.register(r'produtos', ProductViewSet)
+router.register(r'vendas', SaleViewSet)
+router.register(r'estoques', StockViewSet)
+router.register(r'usuarios', UserViewSet)
+router.register(r'ofertas', OfferViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+
 ]
